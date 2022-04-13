6a57b0c94ed0c369d01fa70b2ce172e20a7f7583
emil <e.pargevich@mail.ru>
2022-04-13 15:36:45 +0400

    changes

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import threading
 
 
 # context_initializer.Initializer()
-# threading.Thread(target=context_initializer.DeviceErrorThread).start()
+threading.Thread(target=context_initializer.DeviceErrorThread).start()
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
