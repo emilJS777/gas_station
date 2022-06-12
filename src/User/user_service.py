b2e06a4d99a5b0f871db85d976eaad6510f8af4b
emil <e.pargevich@mail.ru>
2022-06-12 19:04:10 +0400

    changes

--- src/User/user_service.py
+++ src/User/user_service.py
@@ -77,7 +77,7 @@ def user_get_by_id(user_id):
 # GET ALL USER
 def user_get_all(page: int, per_page: int, client_id: int):
     users: dict = user_service_db.get_all(page=page, per_page=per_page, client_id=client_id)
-    for user, index in users['items']:
+    for (user, index) in users['items']:
         print(user, index)
         # user['roles'] = []
         # for role_id in user_role_service_db.get_role_ids_by_user_id(user.id):
