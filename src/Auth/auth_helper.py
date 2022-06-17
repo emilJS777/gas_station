0a694b0a70ed2d965e9c0aa6646cbc72062f7f05
emil <e.pargevich@mail.ru>
2022-06-17 15:13:19 +0400

    asdasdf

--- src/Auth/auth_helper.py
+++ src/Auth/auth_helper.py
@@ -5,6 +5,7 @@ from src import app, mail
 # EMAIL SENDER
 def send_ticket_code_to_email(email_address: str, ticket_code: str, message: str):
     msg = Message('Hello', sender=app.config['MAIL_USERNAME'], recipients=[email_address])
-    msg.html = f"<h3><span style='font-size=12px;'>{message}</span> " \
-               f"<span style='color=blue;'>{str(ticket_code)}</span></h3>"
+    msg.html = f"<h3><span style='font-size: 12px;'>{message}</span> " \
+               f"<span style='color: #fff; padding: 10px; background:#000;'>{str(ticket_code)}</span>" \
+               f"</h3>"
     mail.send(msg)
