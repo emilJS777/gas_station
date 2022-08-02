fb02048b5a9de7edd5951a078e30bd9b3770efa9
emil <e.pargevich@mail.ru>
2022-08-02 14:57:36 +0400

    asdasdf

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from src._general.utils import context_initializer
 import threading
 
 
-context_initializer.Initializer()
-threading.Thread(target=context_initializer.DeviceErrorThread().device_error_search).start()
+# context_initializer.Initializer()
+# threading.Thread(target=context_initializer.DeviceErrorThread().device_error_search).start()
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5001)
 
